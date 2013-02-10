d8b2d86853670236d2e766aa261219dd2c1540e4
Vidit Drolia <viditdrolia@gmail.com>
2013-02-09 16:47:53 -0800

    Change multi-line comment format

--- src/lockservice/client.go
+++ src/lockservice/client.go
@@ -20,22 +20,22 @@ func MakeClerk(primary string, backup string) *Clerk {
   return ck
 }
 
-//
-// call() sends an RPC to the rpcname handler on server srv
-// with arguments args, waits for the reply, and leaves the
-// reply in reply. the reply argument should be the address
-// of a reply structure.
-//
-// call() returns true if the server responded, and false
-// if call() was not able to contact the server. in particular,
-// reply's contents are valid if and only if call() returned true.
-//
-// you should assume that call() will time out and return an
-// error after a while if it doesn't get a reply from the server.
-//
-// please use call() to send all RPCs, in client.go and server.go.
-// please don't change this function.
-//
+/*
+* call() sends an RPC to the rpcname handler on server srv
+* with arguments args, waits for the reply, and leaves the
+* reply in reply. the reply argument should be the address
+* of a reply structure.
+*
+* call() returns true if the server responded, and false
+* if call() was not able to contact the server. in particular,
+* reply's contents are valid if and only if call() returned true.
+*
+* you should assume that call() will time out and return an
+* error after a while if it doesn't get a reply from the server.
+*
+* please use call() to send all RPCs, in client.go and server.go.
+* please don't change this function.
+*/
 func call(srv string, rpcname string,
           args interface{}, reply interface{}) bool {
   c, errx := rpc.Dial("unix", srv)
