efb65a3021df30b741b5c36fb6875e220e0122b1
Vidit Drolia <viditdrolia@gmail.com>
2013-10-12 17:21:00 -0700

    Move private methods to their own section in the code

--- src/viewservice/view_tracker.go
+++ src/viewservice/view_tracker.go
@@ -43,16 +43,6 @@ func (this *ViewTracker) PingTable() *map[string] time.Time {
 func (this *ViewTracker) View() *View {
     return &this.currentView
 }
-
-func elapsedDeadPings(lastPing time.Time) int64 {
-    now   := time.Now()
-    delta := now.Sub(lastPing)
-    return int64(delta/PingInterval)
-    //milli := (delta.Nanoseconds()/1000)
-    //pings := milli / (PingInterval.Nanoseconds()/1000)
-    //return pings
-}
-
 /* tick() is called once per PingInterval.
 - thread safe
 - It should notice
@@ -66,4 +56,15 @@ ii. idle server when there is no backup -> TODO
 */
 func (this  *ViewTracker) Tick() {
   // Your code here.
+}
+
+//private methods
+
+func elapsedDeadPings(lastPing time.Time) int64 {
+    now   := time.Now()
+    delta := now.Sub(lastPing)
+    return int64(delta/PingInterval)
+    //milli := (delta.Nanoseconds()/1000)
+    //pings := milli / (PingInterval.Nanoseconds()/1000)
+    //return pings
 }
